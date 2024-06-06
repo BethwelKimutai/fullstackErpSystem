9448b4f471f1285de833344549afb8bd84c7892a
KimutaiBethwel <bethwel.kimutai@strathmore.edu>
2024-06-06 11:51:21 +0300

    new inventory backend created

---
+++ thefrontend/pages/inventory/products/product/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+  <div class="border rounded-lg p-4 flex justify-between items-center dark:bg-zinc-800">
+    <div class="flex items-center">
+      <img src="https://placehold.co/50x50" alt="Product Image" class="mr-4">
+      <div>
+        <h2 class="font-semibold text-black dark:text-white">ALU. WORKSTATION 2 SEATER 600</h2>
+        <p class="text-zinc-500 dark:text-zinc-400">[HJ1WS04]</p>
+        <p>Price: $ 695.00000000</p>
+        <p>On hand: 0.00000000 Units</p>
+      </div>
+    </div>
+    <button class="text-zinc-500 dark:text-zinc-400">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+      </svg>
+    </button>
+  </div>
+</div>
+</template>
